1483f15dbb14f640ede5b6ba2d4e396c7907f086
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2017-05-12 17:25:48 +0200

    Update timeout callback to respect error attribute #3

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-'use strict';
+"use strict";
 
-module.exports = function(timeout,callback){
-    var called = false;
-    if(typeof timeout === 'function'){
-        callback = timeout;
-        timeout = 10*1000;
-    }
+module.exports = function(timeout, callback) {
+  var called = false;
+  if (typeof timeout === "function") {
+    callback = timeout;
+    timeout = 10 * 1000;
+  }
 
-    var interval = setTimeout(function(){
-        if(called)return;
-        called = true;
-        callback(new Error('callback timeout'));
-    },timeout);
+  var interval = setTimeout(
+    function() {
+      if (called) return;
+      called = true;
+      callback(new Error("callback timeout"));
+    },
+    timeout
+  );
 
-    return function(){
-        if(called)return;
-        called = true;
-        clearTimeout(interval);
-        callback.apply(this,arguments);
-    }
-}
+  return function() {
+    if (called) return;
+    called = true;
+    clearTimeout(interval);
+    const args = Array.prototype.slice.call(arguments);
+    args.unshift(null);
+    callback.apply(this, args);
+  };
+};
--- test/test.js
+++ test/test.js
@@ -1,50 +1,56 @@
-var assert = require('assert');
+var assert = require("assert");
 
-var timeoutCallback = require('../index.js');
+var timeoutCallback = require("../index.js");
 
-describe('test callback timeout',function(){
-    it('should call callback function regularly',function(done){
-        var c = function(err,arg1,arg2){
-            assert.equal(arg1,'aaa');
-            assert.equal(arg2,'bbb');
-            done(err)
-        }
-        var tc = timeoutCallback(c);
-        tc(null,'aaa','bbb');
-    });
+describe("test callback timeout", function() {
+  it("should call callback function regularly", function(done) {
+    var c = function(err, arg1, arg2) {
+      assert.equal(arg1, "aaa");
+      assert.equal(arg2, "bbb");
+      done(err);
+    };
+    var tc = timeoutCallback(c);
+    tc("aaa", "bbb");
+  });
 
-    it('should call callback function with delay',function(done){
-        var c = function(err,arg1,arg2){
-            assert.equal(arg1,'aaa');
-            assert.equal(arg2,'bbb');
-            done(err)
-        }
-        var tc = timeoutCallback(c);
-        setTimeout(function(){
-            tc(null,'aaa','bbb');
-        },200);
-    });
+  it("should call callback function with delay", function(done) {
+    var c = function(err, arg1, arg2) {
+      assert.equal(arg1, "aaa");
+      assert.equal(arg2, "bbb");
+      done(err);
+    };
+    var tc = timeoutCallback(c);
+    setTimeout(
+      function() {
+        tc("aaa", "bbb");
+      },
+      200
+    );
+  });
 
-    it('should timeout callback',function(done){
-        var c = function(err,arg1,arg2){
-            assert.ok(err != null)
-            assert.ok(arg1 == null);
-            assert.ok(arg2 == null);
-            done()
-        }
-        var tc = timeoutCallback(500,c);
-        setTimeout(function(){
-            tc(null,'aaa','bbb');
-        },2000);
-    });
+  it("should timeout callback", function(done) {
+    var c = function(err, arg1, arg2) {
+      assert.ok(err != null);
+      assert.ok(arg1 == null);
+      assert.ok(arg2 == null);
+      done();
+    };
+    var tc = timeoutCallback(500, c);
+    setTimeout(
+      function() {
+        tc("aaa", "bbb");
+      },
+      2000
+    );
+  });
 
-    it('should execute callback immediately',function() {
-      var c = function(err,arg1,arg2){
-          assert.ok(err == null)
-          assert.equal(arg1,'aaa');
-          assert.equal(arg2,'bbb');
-      }
-      var tc = timeoutCallback(500,c);
-      tc(null,'aaa','bbb');
-    })
-})
+  it("should execute callback immediately", function() {
+    var c = function(err, arg1, arg2) {
+      assert.ok(err == null);
+      assert.equal(arg1, "aaa");
+      assert.equal(arg2, "bbb");
+    };
+    var tc = timeoutCallback(500, c);
+    tc("aaa", "bbb");
+  });
+});
